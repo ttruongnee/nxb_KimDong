073bc7bf3a8a3a301418ede6e89e65db948d01c1
ttruongnee <dinhthientruong2192004@gmail.com>
2025-05-07 21:22:24 +0700

    update routes

--- backend/routes/giohang.route.js
+++ backend/routes/giohang.route.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const giohangController = require("../controllers/giohang.controller");
 
-/* GET users listing. */
+
 router.get('/', giohangController.getAll);
 router.get('/:id', giohangController.getById);
 router.post('/', giohangController.insert);
